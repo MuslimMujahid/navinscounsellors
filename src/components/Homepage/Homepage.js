eaf35b753573d74068de4c08cfed8983c3cf20d8
MuslimMujahid <muslimmujahid1712@gmail.com>
2021-02-05 21:11:58 +0700

    create about section on homepage, what and why

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -3,20 +3,51 @@ import './style.scss'
 
 const Homepage = () => {
     return (
-        <div className="hero">
-            <img src="./images/banner.jpg" alt="Navigating Your Legal Needs"/>
-            <div className="caption">
-                <h1>"Navigating Yout Legal Needs"</h1>
+        <div className="homepage">
+            <div className="hero">
+                <img src="./images/banner.jpg" alt="Navigating Your Legal Needs"/>
+                <div className="caption">
+                    <h1>"Navigating Yout Legal Needs"</h1>
 
-                {/* Contact us button */}
-                <button className="contact-us">
-                    <div>Contact</div>
-                    <div>Us</div>
-                </button>
+                    {/* Contact us button */}
+                    <button className="contact-us">
+                        <div>Contact</div>
+                        <div>Us</div>
+                    </button>
+                </div>
+
+                {/* Black Layer */}
+                <div className="layer"></div>
             </div>
-
-            {/* Black Layer */}
-            <div className="layer"></div>
+            
+            {/* Start of about section */}
+            <section className="about">
+                <div className="first-col">
+                    <div className="image">
+                        <img src="/images/building1.jpg" alt="What is NAV|INS Co."/>
+                    </div>
+                    <div className="caption">
+                        <h3>What is NAV|INS Co.</h3>
+                        <p>
+                            NAV|INS Co. is a Law Office established and located in Indonesia. We have professional Lawyers with depth of knowledge and experience in their respective field. Understanding deeply the needs and interests of Our Clients is the way and approach that We take to provide legal services to Our Clients.
+                        </p>
+                    </div>
+                    <div className="layer"></div>
+                </div>
+                <div className="second-col">
+                    <div className="image">
+                        <img src="/images/building2.jpg" alt="Why NAV|INS Co."/>
+                    </div>
+                    <div className="caption">
+                        <h3>Why NAV|INS Co.?</h3>
+                        <p>
+                            We understand that Client’s Legal needs and interests are essential. Therefore, we always give Great Legal Teams who are creative, strong, and expert in their field, in order to be able to provide legal services to Our Clients professionally, in the basis of applicable regulations.
+                        </p>
+                    </div>
+                    <div className="layer"></div>
+                </div>
+            </section>
+            {/* End of about section */}
         </div>
     )
 }
