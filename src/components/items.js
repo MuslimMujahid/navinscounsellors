5ae8f346e07540293f2296535c10087166d9af3f
MuslimMujahid <muslimmujahid1712@gmail.com>
2021-04-05 15:51:17 +0700

    update items

--- src/components/items.js
+++ src/components/items.js
@@ -2,29 +2,120 @@ import WhatsAppIcon from '@material-ui/icons/WhatsApp'
 import InstagramIcon from '@material-ui/icons/Instagram'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 
+const about_en = [
+    `NAV|INS Co. is a law office established and located in Indonesia. We have legal team with depth
+    of knowledge and experience in their field. We are committed to be a reliable partner in
+    providing legal service which is excellent, responsive, practical and commercial oriented.
+    Understanding the needs and interests of our clients business is the approach that we take to
+    provide legal services.`,
+
+    `We understand that legal needs and interests of our clients are essential. Therefore, we provide
+    legal team who are very creative, strong, and expert in their field, in order to be able to provide
+    excellent legal services and commercial oriented to the interest of our clients business, based on
+    applicable regulations.`
+]
+
+const about_id = [
+    `NAV|INS Co. adalah sebuah kantor hukum yang didirikan dan berlokasi di Indonesia. Kami
+    memiliki tim hukum dengan pengetahuan dan pengalaman yang sangat dalam di bidangnya
+    masing-masing. Kami berkomitmen untuk menjadi partner andalan dalam memberikan jasa
+    hukum yang prima, responsif, praktis dan berorientasi komersial. Memahami kebutuhan dan
+    kepentingan bisnis klien kami adalah pendekatan yang kami lakukan dalam memberikan jasa
+    hukum.`,
+
+    `Kami memahami bahwa kebutuhan dan kepentingan hukum klien kami adalah hal yang esensial.
+    Oleh karena itu, Kami menyediakan tim hukum yang sangat kreatif, tangguh dan ahli di
+    bidangnya masing-masing, agar dapat memberikan jasa hukum yang prima dan berorientasi
+    komersil kepada kepentingan bisnis klien kami, dengan tetap berlandaskan pada ketentuan
+    hukum yang berlaku.`
+]
+
 const practice_area_en = [
     {
         title: 'Corporate & Employment',
         image: '/images/practice-area-1-min.jpg',
-        text: 'We provide legal services for area of the Company & Employment among others consisting of: drafting commercial agreement, legal opinion, conducting legal due diligence, transfer of share, acquisition & merger, liquidation, carrying out license or permit arrangements, other general corporate, drafting company regulation &employment contract, as well as handling of labor disputes.',
+        text: `In the field of corporate & employment, we assist/represent our clients to handle the
+                matters among others:`,
+        additional: [
+            `Drafting commercial agreement, providing legal opinion, conducting legal due diligence,
+            assisting transfer of share, acquisition & merger, liquidation, permits arrangements;`,
+
+            `Assisting/representing clients in the area of employment among others: drafting
+            company regulation, employment agreement, foreign employment permits, termination
+            and settlement of labor dispute.`
+        ],
         link: '/practice-area/corporate-employment',
     },
     {
         title: 'Litigation & Dispute Resolution',
         image: '/images/practice-area-2.jpeg',
-        text: 'We are passionate to overcome the issue through practical and commercial solution. We help Our Clients in representing and/or accompanying during process of dispute resolution, whether through out of court settlement process and litigation process.',
+        text: `We are very passionate to settle the legal issue of our clients through practical and
+                commercial solution. For this field, we assist/represent clients during the disputes resolution
+                process among others for the case of civil, criminal, state administration, industrial relation
+                and intellectual property, through outside and inside court settlement process.`,
+        additional: [],
         link: '/practice-area/litigation-dispute-resolution',
     },
     {
         title: 'Commercial Collection & Bankruptcy',
         image: '/images/practice-area-3-min.jpg',
-        text: 'We understand that smooth cash flow is very important in business. Therefore We provide legal services for the field of Commercial Collection & Bankruptcy, to assist Our Clients in conducting collection process against Customers / Debtors both through outside and inside court approaches.',
+        text: `We understand that smooth cash flow is very important in business. Therefore for this field,
+                we assist our clients in conducting collection process against their customers / debtors both
+                through outside and inside court approaches.`,
+        additional: [],
         link: '/practice-area/commercial-collection-bankruptcy',
     },
     {
         title: 'Land & Property',
         image: '/images/practice-area-4.jpg',
-        text: 'We provide legal services for the area of Land and Property, among others land disputes settlement, property disputes settlement, conducting legal due diligence for land and property matters.',
+        text: `For the field of land & property, we assist/represent our clients in conducting legal due
+                diligence and settling the dispute over the land and property matters.`,
+        additional: [],
+        link: '/practice-area/land-property',
+    }
+]
+
+const practice_area_id = [
+    {
+        title: 'Perusahaan & Ketenagakerjaan',
+        image: '/images/practice-area-1-min.jpg',
+        text: `Di bidang perusahaan & ketenagakerjaan, kami membantu/mewakili klien kami dalam
+                melakukan hal-hal antara lain:`,
+        additional: [
+            `Membuat perjanjian, memberikan pendapat hukum, melakukan uji tuntas hukum,
+            membantu peralihan saham, akuisisi & merger, likuidasi, pengurusan perizinan;`,
+
+            `Membantu/mewakili klien untuk bidang ketenagakerjaan antara lain: membuat
+            peraturan perusahaan, kontrak kerja, izin tenaga kerja asing, pengakhiran dan
+            penyelesaian sengketa ketenagakerjaan.`
+        ],
+        link: '/practice-area/corporate-employment',
+    },
+    {
+        title: 'Litigasi & Penyelesaian Sengketa',
+        image: '/images/practice-area-2.jpeg',
+        text: `Kami memiliki semangat untuk menyelesaikan permasalahan hukum klien kami melalui
+                solusi yang praktis dan komersil. Untuk bidang ini, kami membantu/mewakili klien selama
+                proses penyelesaian sengketa antara lain untuk perkara perdata, pidana, tata usaha negara,
+                perselisihan hubungan industrial dan hak kekayaan intelektual baik proses penyelesaian di
+                luar maupun di dalam pengadilan.`,
+        additional: [],
+        link: '/practice-area/litigation-dispute-resolution',
+    },
+    {
+        title: 'Penagihan Piutang & Kepailitan',
+        image: '/images/practice-area-3-min.jpg',
+        text: `Kami memahami bahwa arus kas yang lancar sangat penting dalam sebuah bisnis. Oleh
+                karena itu untuk bidang ini, kami membantu klien kami dalam melakukan proses penagihan
+                terhadap customer/debitur dari klien kami baik melalui pendekatan di luar maupun di dalam
+                Pengadilan.`,
+        link: '/practice-area/commercial-collection-bankruptcy',
+    },
+    {
+        title: 'Tanah & Properti',
+        image: '/images/practice-area-4.jpg',
+        text: `Di bidang tanah & property, kami membantu/mewakili klien kami dalam melakukan uji
+                tuntas hukum dan menangani sengketa atas tanah dan property.`,
         link: '/practice-area/land-property',
     }
 ]
@@ -34,12 +125,26 @@ const lawyer_en = [
         name: 'Muh. Naim Syahrir, S.H., M.H.',
         role: 'Founder and Managing Partner',
         desc: [
-            'Naim is a Founder and Managing Partner of NAV|INS. He is a member of the firm’s litigation practice. His field practice of expertise includes complex and transnational commercial disputes, bankruptcy and court-supervised debt restructuring.',
-            'He has also experience in handling liquidation of company, labor dispute, medical malpractice and compliance issues.'
+            `Prior to establishing NAV|INS Co., Naim was active as a Lawyer in various prominent law office
+            in Jakarta. During his carrier path, he has built up and strengthened his skill and quality in legal
+            matters over many years. He has an extensive experience in handling litigation, corporate
+            matters, commercial collection and bankruptcy.`,
+
+            `During working at his previous law office, Naim has assisted and/or represented diverse
+            companies both local and international companies in relation with litigation and corporate
+            matters, among other assisting/representing : Houghton Singapore, PT Caterpillar Finance
+            Indonesia, PT ABB Sakti Industri, JFE Engineering Corporation, PT Actavis Indonesia, PT Inti
+            Karya Persada Tehnik, PT Petrofac IKPT International, PT Asanusa Asset Manajement, PT
+            Asanusa Tasco Investama and PT Al Amoudi Natural Resource Tradmin.`,
+
+            `The total number of cases which has been handled by Naim for commercial collection, litigation
+            and corporate cases, is approximately 1100 cases, dominated by commercial collection cases.`
         ],
         education: ['Master of Law, University of Indonesia'],
         organization: ['Indonesian Bar Association (PERADI)'],
-        language: ['Bahasa Indonesia', 'English'],
+        language: ['Bahasa', 'English'],
+        email: 'naim@navinscounsellors.com',
+        contact: '021 – 299112275',
         image: '/images/lawyer-1-min.JPG',
         link: '/lawyer/muh-naim-syahrir-sh-mh',
     },
@@ -47,11 +152,18 @@ const lawyer_en = [
         name: 'Febrian Yanata Putra, S.H., M.H.',
         role: 'Partner',
         desc: [
-            'Febriyan is a Partner of NAV|INS. His field practice of expertise includes litigation, bankruptcy, dispute resolution and general corporate.'
+            `Febriyan is a Partner of NAV|INS Co. His expertise includes litigation, bankruptcy, dispute
+            resolution and general corporate matters.`,
+
+            `During working his previous office, Febriyan has represented and/or assisted companies among
+            others: PT Pelindo IV, PT Sinarmas, PT Badja Baru, PT Tekonindo, RS Kedoya Jakarta Barat, PT
+            Reliance, PT Karya Bersama Takarob, PT Nirwana Wastu Pratama and PT Danadipa Aluwung.`
         ],
         education: ['Master of Law, University of Hasanuddin'],
         organization: ['Indonesian Bar Association (PERADI)'],
-        language: ['Bahasa Indonesia', 'English'],
+        language: ['Bahasa', 'English'],
+        email: 'febriyan@navinscounsellors.com',
+        contact: '021 - 299112275',
         image: '/images/lawyer-2.jpeg',
         link: '/lawyer/febriyan-yanata-putra-sh-mh',
     },
@@ -59,11 +171,25 @@ const lawyer_en = [
         name: 'Galih Putra Perdana, S.H.',
         role: 'Partner',
         desc: [
-            'Galih is a Partner of NAV|INS. His field practice of expertise includes litigation (at various different stages during the process) in dispute of civil, criminal, state administration, industrial relations/labor, bankruptcy and suspension of debt payment obligation, copyright, and family law.'
+            `Galih is a Partner of NAV|INS Co. Prior to being a Partner of NAV|INS. Co, he was active as a
+            Lawyer in several prominent law firms in Jakarta. He has extensive legal experience over many
+            years in complicated civil, criminal, and state administrative litigation, industrial dispute
+            resolution, alternative dispute resolution and bankruptcy.`,
+
+            `During working in his previous law firms, Galih has handled various legal services in relation
+            with corporate and commercial litigation, including assisting and/or representing among
+            others: FGV Kalimantan Sdn Bhd, PT Asuransi Jiwa Sequislife, PT Panin Dai-ichi Life, PT Cita Inti
+            Pratama, PT Credo Data Indonesia, PT ABB Sakti Industri, PT Caterpillar Finance Indonesia, PT
+            Pacific Prestress Indonesia, PT Sefas Pelindotama, PT. Sefas Keliantama, PT Malindo Feedmill,
+            Tbk., PT Mulya Agung Dirgantara, CV. Bintang, PT. Dakai Impex, PT Asiaplast Industries, Tbk., PT
+            Vins Inti Perkasa (deVins SkyVilla), PT Wanaraja Putra Perkasa, PT. Bank ANZ Indonesia and
+            Komala’s International Pte Ltd.`
         ],
         education: ['Bachelor Degree of Law, University of Katolik Parahyangan'],
         organization: ['Indonesian Bar Association (PERADI)'],
-        language: ['Bahasa Indonesia', 'English'],
+        language: ['Bahasa', 'English'],
+        email: 'galih@navinscounsellors.com',
+        contact: '021 - 299112275',
         image: '/images/lawyer-3.jpg',
         link: '/lawyer/galih-putra-perdama-sh',
     },
@@ -71,11 +197,103 @@ const lawyer_en = [
         name: 'Fanny Jenifer, S.H.',
         role: 'Associate Lawyer',
         desc: [
-            'Fanny is an Associate Lawyer of NAV|INS. Her field practice of expertise includes general corporate, employment, merger and acquisition, investment, compliance, and cyber law.'
+            `Fanny is Associate of NAV|INS Co. Her expertise includes general corporate, employment,
+            merger and acquisition, investment, legal compliance and cyber.`
         ],
         education: ['Bachelor Degree of Law, University of Padjadjaran'],
         organization: ['Indonesian Bar Association (PERADI)'],
-        language: ['Bahasa Indonesia', 'English'],
+        language: ['Bahasa', 'English'],
+        email: 'fanny@navinscounsellors.com',
+        contact: '021 - 299112275',
+        image: '/images/lawyer-4.jpg',
+        link: '/lawyer/fanny-jenifer-sh',
+    }
+]
+
+const lawyer_id = [
+    {
+        name: 'Muh. Naim Syahrir, S.H., M.H.',
+        role: 'Founder and Managing Partner',
+        desc: [
+            `Sebelum mendirikan NAV|INS. Co., Naim aktif sebagai Lawyer di berbagai kantor hukum
+            terkemuka di Jakarta. Dalam perjalanan karirnya, dia telah membangun dan memperkuat
+            keahlian dan kualitasnya di bidang hukum selama bertahun-tahun. Dia memiliki pengalaman
+            yang sangat luas dalam menangani perkara litigasi, korporasi, penagihan dan kepailitan.`,
+
+            `Selama bekerja di kantor hukum sebelumnya, Naim telah membantu dan/atau mewakili
+            berbagai perusahaan baik lokal maupun internasional sehubungan dengan persoalan litigasi
+            dan korporasi, antara lain membantu/mewakili: Houghton Singapore, PT Caterpillar Finance
+            Indonesia, PT ABB Sakti Industri, JFE Engineering Corporation, PT Actavis Indonesia, PT Inti
+            Karya Persada Tehnik, PT Petrofac IKPT International, PT Asanusa Asset Manajement, PT
+            Asanusa Tasco Investama dan PT Al Amoudi Natural Resource Tradmin.`,
+
+            `Total perkara yang telah ditangani oleh Naim baik perkara penagihan, litigasi dan corporate
+            adalah berjumlah sekitar kurang lebih 1100 perkara yang didominasi perkara penagihan.`
+        ],
+        education: ['Magister Hukum, Universitas Indonesia'],
+        organization: ['Perhimpunan Advokat Indonesia (PERADI)'],
+        language: ['Indonesia', 'Inggris'],
+        email: 'naim@navinscounsellors.com',
+        contact: '021 – 299112275',
+        image: '/images/lawyer-1-min.JPG',
+        link: '/lawyer/muh-naim-syahrir-sh-mh',
+    },
+    {
+        name: 'Febrian Yanata Putra, S.H., M.H.',
+        role: 'Partner',
+        desc: [
+            `Febriyan adalah Partner NAV|INS Co. Bidang keahliannya meliputi litigasi, kepailitan,
+            penyelesaian sengketa dan permasalahan perusaahaan umum.`,
+
+            `Febriyan adalah Partner NAV|INS Co. Bidang keahliannya meliputi litigasi, kepailitan,
+            penyelesaian sengketa dan permasalahan perusaahaan umum.`
+        ],
+        education: ['Magister Hukum, Universitas Hasanuddin'],
+        organization: ['Perhimpunan Advokat Indonesia (PERADI)'],
+        language: ['Indonesia', 'Inggris'],
+        email: 'febriyan@navinscounsellors.com',
+        contact: '021 - 299112275',
+        image: '/images/lawyer-2.jpeg',
+        link: '/lawyer/febriyan-yanata-putra-sh-mh',
+    },
+    {
+        name: 'Galih Putra Perdana, S.H.',
+        role: 'Partner',
+        desc: [
+            `Galih adalah Partner NAV|INS Co. Sebelum menjadi Partner NAV|INS. Co, dia aktif sebagai
+            Lawyer di beberapa firma hukum ternama di Jakarta. Dia memiliki pengalaman hukum yang
+            sangat luas selama bertahun-tahun di bidang perdata, pidana dan litigasi administrasi negara,
+            penyelesaian sengketa ketenagakerjaan, penyelesaian sengketa alternatif, dan kepailitan.`,
+
+            `Selama bekerja di firma hukum sebelumnya, Galih telah menangani berbagai jasa hukum
+            sehubungan dengan korporasi dan litigasi komersil, termasuk membantu dan/atau mewakili
+            antara lain: FGV Kalimantan Sdn Bhd, PT Asuransi Jiwa Sequislife, PT Panin Dai-ichi Life, PT Cita
+            Inti Pratama, PT Credo Data Indonesia, PT ABB Sakti Industri, PT Caterpillar Finance Indonesia,
+            PT Pacific Prestress Indonesia, PT Sefas Pelindotama, PT. Sefas Keliantama, PT Malindo
+            Feedmill, Tbk., PT Mulya Agung Dirgantara, CV. Bintang, PT. Dakai Impex, PT Asiaplast
+            Industries, Tbk., PT Vins Inti Perkasa (deVins SkyVilla), PT Wanaraja Putra Perkasa, PT. Bank ANZ
+            Indonesia dan Komala’s International Pte Ltd.`
+        ],
+        education: ['Sarjana Hukum, Universitas Katolik Parahyangan'],
+        organization: ['Perhimpunan Advokat Indonesia (PERADI)'],
+        language: ['Indonesia', 'Inggris'],
+        email: 'galih@navinscounsellors.com',
+        contact: '021 - 299112275',
+        image: '/images/lawyer-3.jpg',
+        link: '/lawyer/galih-putra-perdama-sh',
+    },
+    {
+        name: 'Fanny Jenifer, S.H.',
+        role: 'Associate Lawyer',
+        desc: [
+            `Fanny adalah Associate dari NAV|INS Co. Bidang keahliannya meliputi persoalan perusahaan
+            umum, ketenagakerjaan, merger dan akuisisi, investasi, kepatuhan hukum dan siber.`
+        ],
+        education: ['Sarjana Hukum, Universitas Padjadjaran'],
+        organization: ['Perhimpunan Advokat Indonesia (PERADI)'],
+        language: ['Indonesia', 'Inggris'],
+        email: 'fanny@navinscounsellors.com',
+        contact: '021 - 299112275',
         image: '/images/lawyer-4.jpg',
         link: '/lawyer/fanny-jenifer-sh',
     }
@@ -99,4 +317,9 @@ const social = [
     }
 ]
 
-export { practice_area_en, lawyer_en, social }
+export {
+    about_en, about_id, 
+    practice_area_en, practice_area_id, 
+    lawyer_en, lawyer_id, 
+    social 
+}
